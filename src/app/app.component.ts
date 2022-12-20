22d6858c39cc0112ce1bb6d14c579e1622a449f1
Jarvis2207 <guilherme.hlg.eng@gmail.com>
2022-12-20 12:30:43 -0300

    finaliza capitulo cuidados com a abstracao do Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Observable} from "rxjs";
+
+import {PhotoBoardService} from "./shared/components/photo-board/services/photo-board.service";
+import {Photo} from "./shared/components/photo-board/interfaces/photo";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public likes = 0;
+  public photos$: Observable<Photo[]>;
+
+  constructor(private service: PhotoBoardService) {}
+
+  public ngOnInit(): void {
+    this.photos$ = this.service.getPhotos();
+  }
 
   public like(): void {
     this.likes++;
